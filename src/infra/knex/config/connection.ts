5d9eb1c65febdf19d27f7fc5c36e00201a448760
Amanda Duarte <amandaduuaarte@gmail.com>
2023-09-20 21:24:21 -0300

    chore: Transforming the connection file to class

--- src/infra/knex/config/connection.ts
+++ src/infra/knex/config/connection.ts
@@ -1,5 +1,20 @@
 import knex from "knex";
 import * as knexConnection from './knexfile';
 
-export const db = knex(knexConnection);
+export class Connection {
+    
+    public db (tableName: string) {
+       return knex(knexConnection)(tableName);
+    }
+
+    public async validateConnection() {
+        this.db('events').then((response) => { 
+            if (response) {
+                console.info('[Ticket-service]-Connection with database was successfully!');
+            }
+        }).catch(error => {
+            console.error('[Ticket-service]-Error in connection with database', error);
+        })
+    }
+}
 
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { validateDBConnection } from '@/infra/knex/validateConnection';
+import { Connection } from '@/infra/knex/config/connection';
 
 const app = express();
 const port = 3000;
+const connection = new Connection();
 
-validateDBConnection();
+connection.validateConnection();
 
 app.listen(port, () => {
   console.log(`Server listening on port:${port}`);
