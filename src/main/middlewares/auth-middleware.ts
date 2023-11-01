b732f3a6ed166e59f56be168950d587e0002fbdc
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-01 18:46:31 -0300

    feat: Implementing factory at middleware

--- src/main/middlewares/auth-middleware.ts
+++ src/main/middlewares/auth-middleware.ts
@@ -1,23 +1,17 @@
-import { BAD_REQUEST } from "@/application/constants";
-
 import { Middleware } from "@/application/interfaces/middleware";
-import { Request, Response } from "express";
+import { error } from "@/application/utils/http";
 
 export class AuthMiddleware implements Middleware {
-  handleRequest(req: Request, res: Response, next: any): void | any {
+  handleRequest(data: any): any {
     try {
-      const { accesstoken } = req.headers;
+      const { accesstoken } = data;
 
       if (!accesstoken) {
         throw new Error("Access token was not provided.");
       }
+      return accesstoken;
     } catch (err: any) {
-      return res.status(BAD_REQUEST).send({
-        code: BAD_REQUEST,
-        message: err.message,
-      });
+      return error(err.message);
     }
-
-    next();
   }
 }
