2ad1a2b8c706e2e7939e2673bb943c7bcac48c65
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-01 17:47:18 -0300

    fix: Resolving response body

--- src/main/adpters/adpter-route.ts
+++ src/main/adpters/adpter-route.ts
@@ -1,14 +1,14 @@
 import { Controller } from "@/application/interfaces";
-import { error, success } from "@/application/utils/http";
+
 import { Request, Response } from "express";
 
 export const AdpterRouter = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const content = await controller.handleRequest(req.body);
-    res.send(success(content));
+    res.send(content);
 
     if (!content) {
-      res.send(error(content));
+      res.send(content);
     }
   };
 };
