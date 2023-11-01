6b82f7930f0ca574b56dded6418b78cc0ce28d85
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-01 19:04:26 -0300

    fix: Tuning the auth middleware factory.

--- src/main/middlewares/auth-middleware.ts
+++ src/main/middlewares/auth-middleware.ts
@@ -2,14 +2,13 @@ import { Middleware } from "@/application/interfaces/middleware";
 import { error } from "@/application/utils/http";
 
 export class AuthMiddleware implements Middleware {
-  handleRequest(data: any): any {
+  async handleRequest(data: any): Promise<any> {
     try {
       const { accesstoken } = data;
 
       if (!accesstoken) {
         throw new Error("Access token was not provided.");
       }
-      return accesstoken;
     } catch (err: any) {
       return error(err.message);
     }
